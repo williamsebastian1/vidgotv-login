f77e9b9cc4e962e3edac25496607b67ae9ad8414
williamsebastian1 <williamsebastian2024@outlook.com>
2025-06-25 15:14:17 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'Vidgotv Login'
+author = 'Denial'
+release = '1.0'
+ 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+ 
+html_theme = 'alabaster'
+html_static_path = ['_static']   
+ 
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '_static/vidgo-tv.jpg'
